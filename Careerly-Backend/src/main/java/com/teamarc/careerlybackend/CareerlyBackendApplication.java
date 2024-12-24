a6139ff4c9c7b941a6265ab98f7dfc6b7eec232a
Yash Zade <yashmzade@gmail.com>
2024-12-24 16:56:58 +0530

    Initial Setup

---
+++ Careerly-Backend/src/main/java/com/teamarc/careerlybackend/CareerlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamarc.careerlybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerlyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerlyBackendApplication.class, args);
+    }
+
+}
---
+++ Careerly-Backend/src/test/java/com/teamarc/careerlybackend/CareerlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamarc.careerlybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerlyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
