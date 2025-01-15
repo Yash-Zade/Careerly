0e398a8df11c82fdfcf83bee39c4bb3fa4322235
Yash Zade <yashmzade@gmail.com>
2025-01-15 13:31:45 +0530

    Modify way to split Token

--- Careerly-Backend/src/main/java/com/teamarc/careerlybackend/security/JwtAuthFilter.java
+++ Careerly-Backend/src/main/java/com/teamarc/careerlybackend/security/JwtAuthFilter.java
@@ -39,7 +39,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 return;
             }
 
-            String token = requestTokenHeader.split("Bearer ")[1];
+            String token = requestTokenHeader.substring(7).trim();
             Long userId = jwtService.getUserIdFromToken(token);
 
             if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
